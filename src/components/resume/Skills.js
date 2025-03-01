f2407dbc1448868aafdcc681c029c4d4c592a059
DhruvAmbaliya <dhruvambaliya72@gmail.com>
2025-03-01 19:01:40 +0530

    Fix : Width

--- src/components/resume/Skills.js
+++ src/components/resume/Skills.js
@@ -16,14 +16,14 @@ const Skills = () => {
         <div className="py-4">
           <div className="py-3 border-b-[1px] border-zinc-800">
             <p className="text-base text-textColor -mb-1.5">Web Development</p>
-            <span className="w-full bg-zinc-600 h-1 inline-flex relative">
+            <span className="w-full max-sm:w-[75%] bg-zinc-600 h-1 inline-flex relative">
               <span className="w-[95%] h-full absolute top-0 left-0 bg-designColor"></span>
             </span>
           </div>
           {/* web Development */}
           <div className="py-3 border-b-[1px] border-zinc-800">
             <p className="text-base text-textColor -mb-1.5">Web Design</p>
-            <span className="w-full bg-zinc-600 h-1 inline-flex relative">
+            <span className="w-full max-sm:w-[75%] bg-zinc-600 h-1 inline-flex relative">
               <span className="w-[90%] h-full absolute top-0 left-0 bg-designColor"></span>
             </span>
           </div>
@@ -32,14 +32,14 @@ const Skills = () => {
             <p className="text-base text-textColor -mb-1.5">
               Mobile Application
             </p>
-            <span className="w-full bg-zinc-600 h-1 inline-flex relative">
+            <span className="w-full max-sm:w-[75%] bg-zinc-600 h-1 inline-flex relative">
               <span className="w-[85%] h-full absolute top-0 left-0 bg-designColor"></span>
             </span>
           </div>
           {/* UI Design */}
           <div className="py-3 border-b-[1px] border-zinc-800">
             <p className="text-base text-textColor -mb-1.5">UX Design</p>
-            <span className="w-full bg-zinc-600 h-1 inline-flex relative">
+            <span className="w-full max-sm:w-[75%] bg-zinc-600 h-1 inline-flex relative">
               <span className="w-[80%] h-full absolute top-0 left-0 bg-designColor"></span>
             </span>
           </div>
@@ -54,21 +54,21 @@ const Skills = () => {
         <div className="py-4">
           <div className="py-3 border-b-[1px] border-zinc-800">
             <p className="text-base text-textColor -mb-1.5">English</p>
-            <span className="w-full bg-zinc-600 h-1 inline-flex relative">
+            <span className="w-full max-sm:w-[75%] bg-zinc-600 h-1 inline-flex relative">
               <span className="w-[80%] h-full absolute top-0 left-0 bg-designColor"></span>
             </span>
           </div>
           {/* Hindi */}
           <div className="py-3 border-b-[1px] border-zinc-800">
             <p className="text-base text-textColor -mb-1.5">Hindi</p>
-            <span className="w-full bg-zinc-600 h-1 inline-flex relative">
+            <span className="w-full max-sm:w-[75%] bg-zinc-600 h-1 inline-flex relative">
               <span className="w-[100%] h-full absolute top-0 left-0 bg-designColor"></span>
             </span>
           </div>
           {/* Arabic*/}
           <div className="py-3 border-b-[1px] border-zinc-800">
             <p className="text-base text-textColor -mb-1.5">Gujarati</p>
-            <span className="w-full bg-zinc-600 h-1 inline-flex relative">
+            <span className="w-full max-sm:w-[75%] bg-zinc-600 h-1 inline-flex relative">
               <span className="w-[100%] h-full absolute top-0 left-0 bg-designColor"></span>
             </span>
           </div>
@@ -91,28 +91,28 @@ const Skills = () => {
         <div className="py-4">
           <div className="py-3 border-b-[1px] border-zinc-800">
             <p className="text-base text-textColor -mb-1.5">JavaScript</p>
-            <span className="w-full bg-zinc-600 h-1 inline-flex relative">
+            <span className="w-full max-sm:w-[75%] bg-zinc-600 h-1 inline-flex relative">
               <span className="w-[95%] h-full absolute top-0 left-0 bg-designColor"></span>
             </span>
           </div>
           {/* web Development */}
           <div className="py-3 border-b-[1px] border-zinc-800">
             <p className="text-base text-textColor -mb-1.5">Nodejs</p>
-            <span className="w-full bg-zinc-600 h-1 inline-flex relative">
+            <span className="w-full max-sm:w-[75%] bg-zinc-600 h-1 inline-flex relative">
               <span className="w-[90%] h-full absolute top-0 left-0 bg-designColor"></span>
             </span>
           </div>
           {/* Mobile Application */}
           <div className="py-3 border-b-[1px] border-zinc-800">
             <p className="text-base text-textColor -mb-1.5">Reactjs</p>
-            <span className="w-full bg-zinc-600 h-1 inline-flex relative">
+            <span className="w-full max-sm:w-[75%] bg-zinc-600 h-1 inline-flex relative">
               <span className="w-[85%] h-full absolute top-0 left-0 bg-designColor"></span>
             </span>
           </div>
           {/* UI Design */}
           <div className="py-3 border-b-[1px] border-zinc-800">
             <p className="text-base text-textColor -mb-1.5">TypeScript</p>
-            <span className="w-full bg-zinc-600 h-1 inline-flex relative">
+            <span className="w-full max-sm:w-[75%] bg-zinc-600 h-1 inline-flex relative">
               <span className="w-[80%] h-full absolute top-0 left-0 bg-designColor"></span>
             </span>
           </div>
